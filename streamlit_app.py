0fc1a2d852fabaf5c13a049b34f1385f9ab86056
Xeonanda <161570607+Xeonanda@users.noreply.github.com>
2024-12-21 09:00:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import streamlit as st
 import json
